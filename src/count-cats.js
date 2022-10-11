6588ddaa91e463e989651606056404b661674d88
pepsif <pepsi_fack@ukr.net>
2022-10-11 07:27:42 +0300

    feat: edit count-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Given matrix where you have to find cats by ears "^^"
@@ -12,13 +12,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  *  [0, '^^', 2],
  *  ['^^', 1, 2]
  * ]) => 3`
- *
+ *General task description
+Your task is to write several functions that are the solution to the subtasks. Subtasks descriptions, as well as instructions on how to run tests and submit solutions are below.
  */
-function countCats(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function countCats( backyard ) {
+ 
+  const arr = backyard;
+  let countCat = 0;
+  
+  let findCatEl = arr.forEach( el => {
+   if( el.includes('^^') ) { 
+     countCat++;
+     console.log('catch ' + countCat) }
+  });
+  if(countCat === 0 ) { return 0 }
+  
+     return countCat
 }
 
 module.exports = {
   countCats
 };
+
+console.log( countCats( [ [0, 1, '^^'], [0, '^^', 2], ['^^', 1, 2] ]) )
